9eae226666e353b1602fe3a6057c371abeaf7467
AimallKa <84224340+MiKalla@users.noreply.github.com>
2021-09-17 11:29:44 +0900

    Update talk.js

--- talk.js
+++ talk.js
@@ -2,7 +2,17 @@ $(function()
 {
     $('#timer-holder').click(function()
     {
-        const number = Math.ceil(Math.random()*2);
+        const talk = Math.ceil(Math.random()*3);
+        if(talk == 1)
+        {
+            $('#timer-holder').attr('src','images/timer-holder3.png').fadeIn('srow');
+        }
+        if(talk == 2)
+        {
+            $('#timer-holder').attr('src','images/timer-holder2.png').fadeIn('srow');
+        }
+
+        const number = Math.ceil(Math.random()*3);
         if(number == 1)
         {
             $('#talk').html('どうしたの？');
@@ -10,6 +20,10 @@ $(function()
         if(number == 2)
         {
             $('#talk').html('今日もお勉強するの？');
+        }
+        if(number == 3)
+        {
+            $('#talk').html('勉強するなら、「勉強はじめる」ボタンを押してね！');
         }
 
     });
